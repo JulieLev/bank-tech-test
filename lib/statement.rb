d1ae6dc47482e147104c466e5ede9b1daa549025
JulieLev <jkgee1@yahoo.co.uk>
2016-10-11 16:02:07 +0100

    refactored statement

--- lib/statement.rb
+++ lib/statement.rb
@@ -9,19 +9,32 @@ class Statement
     @account = account
   end
 
+  def statement_header
+    "date || credit || debit || balance"
+  end
+
+  def format_date(date)
+    date.strftime("%d/%m/%Y")
+  end
+
+  def format_amount(amount)
+    sprintf('%.2f', amount)
+  end
+
   def show_statement
-    puts "date || credit || debit || balance"
+    puts statement_header
     total = 0
     n = 1
     while n <= @account.transactions.length
       @account.transactions.map do |transaction|
-        date = transaction[0].strftime("%d/%m/%Y")
+        date = format_date(transaction[0])
         credit_amount = transaction[1].abs
         debit_amount = transaction[2].abs
         total += (credit_amount - debit_amount)
-        credit = sprintf('%.2f', credit_amount )
-        debit = sprintf('%.2f', debit_amount)
-        balance = sprintf('%.2f', total)
+        date = format_date(transaction[0])
+        credit = format_amount(credit_amount)
+        debit = format_amount(debit_amount)
+        balance = format_amount(total)
         puts "#{date} || #{credit} || #{debit} || #{balance}"
         n += 1
       end
