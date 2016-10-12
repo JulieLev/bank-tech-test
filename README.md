a725f00fba2dfdb18d0104a23a59bd7d21dcd214
JulieLev <jkgee1@yahoo.co.uk>
2016-10-12 01:57:29 +0100

    updates ReadMe to show test detail

--- README.md
+++ README.md
@@ -1,3 +1,28 @@
-# bank-tech-test
-Makers Academy - practice tech test (command line bank account)
-More refactoring to be done...
+# The Bank Tech Test
+Makers Academy - practice tech test
+
+### Requirements
+* Deposits, withdrawal
+* Account statement (date, amount, balance)
+* Statement printing
+
+### Acceptance criteria
+
+**Given** a client makes a deposit of 1000 on 10-01-2012  
+**And** a deposit of 2000 on 13-01-2012  
+**And** a withdrawal of 500 on 14-01-2012  
+**When** she prints her bank statement  
+**Then** she would see  
+
+
+```
+date || credit || debit || balance
+14/01/2012 || 0.00 || 500.00 || 2500.00
+13/01/2012 || 2000.00 || 0.00 || 3000.00
+10/01/2012 || 1000.00 || 0.00 || 1000.00
+```
+
+### Additional extensions
+
+* Statement filters (just deposits, withdrawals, date ascending, date descending)
+* Graphical interface
--- spec/models/statement_spec.rb
+++ spec/models/statement_spec.rb
@@ -3,16 +3,16 @@ require 'date'
 
 describe 'Statement' do
   let(:account) { double :bank_account }
-  let(:transaction_1) { double :transaction, credit: 100, debit: 0, date: Date.new(2012, 01, 14), total: 100 }
-  let(:transaction_2) { double :transaction, credit: 0, debit: 20, date: Date.new(2012, 01, 14), total: -20 }
+  let(:transaction_1) { double :transaction, credit: 1000, debit: 0, date: Date.new(2012, 01, 10), total: 1000 }
+  let(:transaction_2) { double :transaction, credit: 0, debit: 500, date: Date.new(2012, 01, 14), total: -500 }
   subject(:statement) { Statement.new(account) }
 
   describe '#show_statement' do
     it "should print a simple statement when run" do
       allow(statement.account).to receive(:transactions).and_return([transaction_1, transaction_2])
       expect { statement.show_statement }.to output("date || credit || debit || balance
-14/01/2012 || 100.00 || 0.00 || 100.00
-14/01/2012 || 0.00 || 20.00 || 80.00\n").to_stdout
+10/01/2012 || 1000.00 || 0.00 || 1000.00
+14/01/2012 || 0.00 || 500.00 || 500.00\n").to_stdout
     end
   end
 end
